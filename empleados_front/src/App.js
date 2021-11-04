a9c3ec577a9344a8225f57616ce306d58af052a9
Diego-Vega <ferzux.09@gmail.com>
2021-11-03 21:48:28 -0500

    se agregó al front un componente

--- empleados_front/src/App.js
+++ empleados_front/src/App.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Login from "./Components/Login";
 
 function App() {
     return (
@@ -8,44 +9,7 @@ function App() {
             <Container>
                 <Row className="justify-content-md-center">
                     <Col md="auto">
-                        <Form>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="formBasicEmail"
-                            >
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Enter email"
-                                />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone
-                                    else.
-                                </Form.Text>
-                            </Form.Group>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="formBasicPassword"
-                            >
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Password"
-                                />
-                            </Form.Group>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="formBasicCheckbox"
-                            >
-                                <Form.Check
-                                    type="checkbox"
-                                    label="Check me out"
-                                />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
+                        <Login/>
                     </Col>
                 </Row>
             </Container>

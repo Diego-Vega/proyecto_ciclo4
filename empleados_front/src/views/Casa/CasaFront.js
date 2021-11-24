eba25248908f27337ccde1dc5a549e62e9455c5c
GustavoRamirez514 <tavo200079.gr@gmail.com>
2021-11-24 10:32:38 -0500

    Casa sin vue xd

--- empleados_front/src/views/Casa/CasaFront.js
+++ empleados_front/src/views/Casa/CasaFront.js
@@ -2,13 +2,10 @@ import "./HomeStyle.css";
 
 const Casa = () => {
   return (
-<v-container>
-    <v-card color="#ccc" className="text-center mx-auto">
+      <div>
       <h1 className="titulo">Bienvenidos!</h1>
       <img className="imagen" src="http://cdn.shopify.com/s/files/1/0043/6384/5720/articles/tienda-abarrotes-001_1024x1024.jpg?v=1571777316"></img>
-    </v-card>
-  </v-container>
-
+      </div>
   
   );
 

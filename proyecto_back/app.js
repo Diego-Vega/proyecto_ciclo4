c999dee24fb75689d9df8ee8694db145266b4265
Diego-Vega <ferzux.09@gmail.com>
2021-12-02 17:58:31 -0500

    intento heroku

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -36,13 +36,9 @@ app.use(function (req, res, next) {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname));
-    app.use(express.static(path.join(__dirname, "build")));
-    app.get("/ping", function (req, res) {
-        return res.send("pong");
-    });
-    app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "build", "index.html"));
+    app.use(express.static(__dirname + "/build/"));
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname + "/build/index.html");
     });
 }
 
